9b6ce8821d95fd3186266f7e9be8f0ec12c1f728
cboudereau <cboudereau@dev-mdossantos.siriona.com>
2015-07-29 13:15:35 +0200

    async state computation expressioné

--- ComputationExpression/WorkflowSample.fsx
+++ ComputationExpression/WorkflowSample.fsx
@@ -7,30 +7,41 @@ type State<'a> =
     | Failure of FailureState
 
 type StateBuilder()=
-    member __.Bind(x, f) = 
-        match x with
-        | Success a -> f a
-        | Failure b -> Failure b
-    member __.Return(x) = Success x
-    member __.ReturnFrom(x) = x
+    member __.Bind(asyncValue, f) = 
+        async {
+                let! value = asyncValue
+                match value with
+                | Success a -> return! f a
+                | Failure b -> return Failure b }
+        
+    member __.Return(x) = async { return Success x }
+    member __.ReturnFrom(x) = async { return! x }
 
 let state = StateBuilder()
 
 let send request response = 
-    match request with
-    | "s" -> Success response
-    | v -> Failure (CommunicationFailure v)
+    async {
+        match request with
+        | "s" -> return Success response
+        | v -> return Failure (CommunicationFailure v) }
 
 let parse response =
-    match response with
-    | "s" -> Success true
-    | v -> Failure (ParsingFailure true)
+    async {
+        match response with
+        | "s" -> return Success true
+        | _ -> return Failure (ParsingFailure true) }
 
-let result = 
+let resultAsync = 
     state {
         let! r1 = send "s" "s"
         return! parse r1 }
 
-match result with
-| Success v -> printfn "Yeah! Success %b" v
-| other -> printfn "Ohhhh No! %A" other
+
+let result = 
+    async {
+        let! result = resultAsync
+        match result with
+        | Success v -> printfn "Yeah! Success %b" v
+        | other -> printfn "Ohhhh No! %A" other }
+
+result |> Async.RunSynchronously
