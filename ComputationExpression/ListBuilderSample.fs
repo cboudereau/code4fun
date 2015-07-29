4d6cb47a75e3a27c51206b5869cbfb5f1bbed565
cboudereau <cboudereau@dev-mdossantos.siriona.com>
2015-07-29 16:54:20 +0200

    worlflow update

---
+++ ComputationExpression/ListBuilderSample.fs
@@ -0,0 +1,30 @@
+﻿module ListBuilderSample
+
+type ListBuilder() =
+    member this.Bind(m, f) = 
+        m |> List.collect f
+
+    member this.Zero() = 
+        printfn "Zero"
+        []
+
+    member this.Return(x) = 
+        printfn "Return an unwrapped %A as a list" x
+        [x]
+
+    member this.Yield(x) = 
+        printfn "Yield an unwrapped %A as a list" x
+        [x]
+        
+    member this.For(m,f) =
+        printfn "For %A" m
+        this.Bind(m,f)
+
+// make an instance of the workflow                
+let listbuilder = new ListBuilder()
+
+listbuilder { 
+    let! x = [1..3]
+    let! y = [10;20;30]
+    return x + y
+} |> printfn "Result :%A"
--- ComputationExpression/WorkflowSample.fsx
+++ ComputationExpression/WorkflowSample.fsx
@@ -1,47 +1,53 @@
 ﻿type FailureState = 
     | CommunicationFailure of string
     | ParsingFailure of bool
+    | ExceptionFailure of exn
 
 type State<'a> = 
     | Success of 'a
     | Failure of FailureState
 
-type StateBuilder()=
-    member __.Bind(asyncValue, f) = 
+module AsyncState = 
+    let asyncBind f asyncValue = 
         async {
-                let! value = asyncValue
-                match value with
-                | Success a -> return! f a
-                | Failure b -> return Failure b }
+            let! value = asyncValue
+            match value with
+            | Success a -> return! f a
+            | Failure b -> return Failure b }
+        
+
+type AsyncStateBuilder()=
+    member __.Bind(asyncValue, f) = AsyncState.asyncBind f asyncValue
         
     member __.Return(x) = async { return Success x }
     member __.ReturnFrom(x) = async { return! x }
 
-let state = StateBuilder()
+    member __.Yield(x) = async{ return! x }
+    member __.For(x, f) = x |> Seq.map (AsyncState.asyncBind f)
 
-let send request response = 
-    async {
-        match request with
-        | "s" -> return Success response
-        | v -> return Failure (CommunicationFailure v) }
+let asyncState = AsyncStateBuilder()
 
+let send requests response = 
+    requests
+    |> Seq.map(
+        function
+        | "s" -> async { return response |> Success }
+        | v -> async { return Failure (CommunicationFailure v)  })
+    
 let parse response =
     async {
+        printfn "Enter parse with response : %s" response
         match response with
         | "s" -> return Success true
         | _ -> return Failure (ParsingFailure true) }
 
-let resultAsync = 
-    state {
-        let! r1 = send "s" "s"
-        return! parse r1 }
+let asyncResults requests response = 
+    asyncState { for s in send requests response do yield parse s  }
+    |> Async.Parallel
 
+let displayResults result = 
+    match result with
+    | Success v -> printfn "Yeah! Success %b" v
+    | other -> printfn "Ohhhh No! %A" other
 
-let result = 
-    async {
-        let! result = resultAsync
-        match result with
-        | Success v -> printfn "Yeah! Success %b" v
-        | other -> printfn "Ohhhh No! %A" other }
-
-result |> Async.RunSynchronously
+asyncResults ["i";"s"] "s" |> Async.RunSynchronously |> Seq.map displayResults |> Seq.toList
