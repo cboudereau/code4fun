3d0323d1c9dafd651c970df2bdb7f9ec88288c91
cboudereau <cboudereau@gmail.com>
2015-08-13 19:54:57 +0200

    actor model

---
+++ ComputationExpression/ActorModel.fsx
@@ -0,0 +1,24 @@
+﻿open Microsoft.FSharp.Control
+
+type Actor<'a> = MailboxProcessor<'a>
+
+let actor = 
+    Actor.Start <| fun inbox -> 
+        let rec listen() = 
+            async {
+                let! (replyChannel, message) = inbox.Receive()
+                replyChannel message
+                do! listen()
+            }
+        listen()
+
+
+let messages = 
+    [ (1, "H1", "Message1")
+      (2, "H1", "Message2")
+      (2, "H2", "Message1")
+      (2, "H1", "Message3") ]
+
+let post message = actor.PostAndAsyncReply <| fun replyChannel -> (replyChannel.Reply, message)
+
+post "Hello" |> Async.RunSynchronously
---
+++ ComputationExpression/AsyncSeqStateWorkflow.fsx
@@ -0,0 +1,24 @@
+﻿type State<'a> = 
+    | Success of 'a
+    | Failure
+
+module State = 
+    let bind f x = 
+        x 
+        |> Seq.map(fun x' -> 
+            async {
+                let! x'' = x'
+                return
+                    match x'' with
+                    | Success v -> f v
+                    | Failure -> Failure            
+            })
+
+    let result x = async { return Success x }
+
+type StateBuilder () =
+    member __.Bind (x, f) = State.bind f x
+    member __.Yield (x) = State.result x
+    member __.YieldFrom(x) = x
+
+let state = StateBuilder()
